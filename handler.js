d7d72f9c753c2c7ee02eba4340a84a7a3a61fcd3
ankit mishra <ankitmishra@ankits-MacBook-Air.local>
2020-11-29 14:06:50 +0530

    query optimization

--- handler.js
+++ handler.js
@@ -3,6 +3,14 @@ const { recordsModel } = require('./model');
 async function getRecords({ startDate, endDate, minCount, maxCount }) {
   const records = await recordsModel.aggregate([
     {
+      $match: {
+        createdAt: {
+          $gt: new Date(startDate),
+          $lt: new Date(endDate),
+        },
+      }
+    },
+    {
       $addFields: {
         totalCount: {
           $sum: '$counts',
@@ -14,11 +22,7 @@ async function getRecords({ startDate, endDate, minCount, maxCount }) {
         totalCount: {
           $gt: minCount,
           $lt: maxCount,
-        },
-        createdAt: {
-          $gt: new Date(startDate),
-          $lt: new Date(endDate),
-        },
+        }
       },
     },
     {
